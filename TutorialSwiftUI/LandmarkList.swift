9023265181a2b5255a862084cd7335b09a4588ae
Fauzi <fauzibinfaisal@gmail.com>
2020-11-22 23:06:14 +0700

    learned make Dynamic List and previewing in diferent devices

---
+++ TutorialSwiftUI/LandmarkList.swift
@@ -0,0 +1,32 @@
+//
+//  LandmarkList.swift
+//  TutorialSwiftUI
+//
+//  Created by Fauzi Fauzi on 22/11/20.
+//  Copyright © 2020 defauloper. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max", "iPad Pro (9.7-inch)"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
