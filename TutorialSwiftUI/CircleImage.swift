2b50504fef6f327806772238ba4fa77c16cc5af4
Fauzi <fauzibinfaisal@gmail.com>
2020-11-22 22:17:40 +0700

    Combining view completed

--- TutorialSwiftUI/CircleImage.swift
+++ TutorialSwiftUI/CircleImage.swift
@@ -11,15 +11,15 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
     }
 }
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
         CircleImage()
-            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-            .overlay(
-                Circle().stroke(Color.white, lineWidth: 4))
-            .shadow(radius: 10 )
     }
 }
